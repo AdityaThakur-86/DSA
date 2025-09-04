065ab5f1f693e75d5d2e036dbdc7d7f27eab4f28
AdityaThakur-86 <aditya102245rishu@gmail.com>
2025-09-04 16:48:44 +0530

    DAY1

---
+++ DAY1.cpp
@@ -0,0 +1,15 @@
+// CALCULATOR
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;  
+    cout<<"Enter first number : ";
+    cin>>a;
+    cout<<"Enter second number : ";
+    cin>>b;
+    cout<<"The sum is : "<< (a+b)<< endl;  //ADDITION
+    cout<<"The difference is :"<< (a-b)<< endl; //SUBSTRACTION
+    cout<<"The product is :"<< (a*b)<< endl; //MULTIPLICATION
+    cout<<"The quotient is :"<< (a/b)<< endl; //DIVISION
+return 0;
+}
